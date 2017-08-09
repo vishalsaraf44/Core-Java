9b682f2a4d3d8cacfad174f6d4bc22e95fbaee82
Vishal <vishalsaraf44@gmail.com>
2017-08-09 20:53:12 +0530

    removed public

--- Cab_Java/main.java
+++ Cab_Java/main.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import java.sql.*;
 import java.util.*;
 
-public class mainlistener implements ActionListener
+class mainlistener implements ActionListener
 { 
 	public void actionPerformed(ActionEvent e) 	
 	{
